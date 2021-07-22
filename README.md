b5262e473b677dbd51b04d1cf0d593f6e574fc98
Hilary <hilary.seselja@gmail.com>
2021-07-22 13:19:47 +1000

    Initial commit.

---
+++ README.md
@@ -0,0 +1,4 @@
+# BGA Guide navigation
+
+[Base Business.gov.au Prototype](https://ausgov.github.io/bga-base-prototype/index.html)
+    
---
+++ _site/js/prototype.js
@@ -0,0 +1,191 @@
+/*jshint browser: true, devel: true, jquery: true*/
+
+$(document).ready(function () {
+    
+    // Main navigation functionality
+    $('.navbar-nav .nav-item.dropdown').on('click', function(){
+        $(this).find('.navigation-first-level-menu').toggleClass('show');
+    });
+    
+    
+    // Prevent click empty 'a' tag from causing scrolling
+    $('a').on('click', function(e){
+        if (! $(this).attr('href') ) {
+            e.preventDefault();
+        }
+    });
+    
+    // Hide empty breadcrumb links and arrows
+    $('a.breadcrumb-link').each(function(){
+        if( $(this).is(':empty') ) {
+            var wrapper = $(this).parent('.breadcrumb-home-wrapper');
+            $(wrapper).css('display', 'none');
+        }
+    });
+    
+    
+    // Header navigation links   
+
+    
+    /*----------- Add side-menu (sticky_list) functionality ----------- */
+    
+    function create_id(text){
+        var text_no_num = text.replace(/[0-9]/g, ''),
+            text_no_punctuation = text_no_num.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()?\']/g,''),
+            final_text = text_no_punctuation.trim();
+        
+        var a_lower_text = final_text.replace(/\s+/g, '-').toLowerCase();
+        return(a_lower_text);
+    }
+
+    // Function for menu stickiness on scroll (called within the if .anchor-menu .sticky-container exists block)
+    function add_position(positions) {
+
+        for (var i = 0; i < positions.length; i++) {
+            var top_position = positions[i];
+            if ($(window).scrollTop() >= top_position) {
+                $('.anchor-menu a').removeClass('active-sticky');
+                $('.anchor-menu a[data-value=' + positions[i] + ']').addClass('active-sticky');
+            }
+        }
+    }
+    
+    
+    // Function to make the side menu sticky
+    var stickyPosition = $('.anchor-menu').offset(); //This var is outside the function because it needs to be determined BEFORE window resizing,.
+    function menuStickiness() {
+        
+        var win = $(window),
+            stickyWidth = $('.twoCol39-left').width();
+        
+        // Set side-menu initial horizontal position 
+        if(win.width() < 575) {
+            $('.anchor-menu').css('position', 'relative').css('top', 'auto');
+        } else if (win.width() >= 575) {
+            if (win.scrollTop() >= stickyPosition.top) {
+                $('.anchor-menu').css('position', 'fixed').css('top', '32px').css('width', stickyWidth);
+            } else {
+                $('.anchor-menu').css('position', 'relative').css('top', 'auto').css('width', stickyWidth);
+            }
+        } 
+        
+        // Reset side-menu position on scroll
+        $(window).scroll(function () {
+
+            stickyWidth = $('.twoCol39-left').width();
+
+            if (win.width() < 575) {
+                $('.anchor-menu').css('position', 'relative').css('top', 'auto').css('width', stickyWidth);
+            } else if (win.width() >= 575) {
+                if (win.scrollTop() >= stickyPosition.top) {
+                    $('.anchor-menu').css('position', 'fixed').css('top', '32px').css('width', stickyWidth);
+                } else if (win.scrollTop() < stickyPosition.top) {
+                    $('.anchor-menu').css('position', 'relative').css('top', 'auto').css('width', stickyWidth);
+                }
+            }
+        });
+    }
+
+    
+    if ($( ".anchor-menu .sticky-container" ).length) {
+        
+        // Get text from each sticky list a-tag and convert it into an id.
+        // Replace spaces with hyphens and remove numerical characters & punctuation at the start where necessary       
+        var sticky_list_2 = {};
+        $('.anchor-menu a').each(function(){
+            var a_text = $(this).text(),
+                text_no_num = a_text.replace(/[0-9]/g, ''),
+                text_no_punctuation = text_no_num.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()?\']/g,''),
+                final_text = text_no_punctuation.trim();
+        
+            var a_lower_text = final_text.replace(/\s+/g, '-').toLowerCase();
+            sticky_list_2[a_text] = a_lower_text;
+            
+        }); 
+
+        // Apply menu stickiness
+        menuStickiness();
+    
+        
+        // Side menu scroll to section of the page
+        // and add top position of element to anchor link as a data-value
+        $('.anchor-menu a').each(function(){
+            
+            var a_text = $(this).text(),
+                element_id = '#' + sticky_list_2[a_text],
+                element_position = $(element_id).offset();
+            
+            //console.log(element_id);
+            
+            if ($(element_id).length){
+                //console.log(element_id);
+                //console.log(element_position);
+                $(this).attr('data-value', Math.round(element_position.top));
+        
+                $(this).on('click', function(){
+                    $([document.documentElement, document.body]).animate(
+                        { scrollTop: $(element_id).offset().top }, 400);
+                    $('.anchor-menu a').removeClass('active-sticky');
+                    $(this).addClass('active-sticky');
+                });
+            }
+        });   
+        
+    
+        // Change menu active state on scroll to different sections of the page
+        var positions = [];
+        $('.anchor-menu a').each(function(){
+            var element_position = $(this).attr('data-value');
+            positions.push(Math.round(element_position));
+        }); 
+    
+        $(window).scroll(function(){
+            add_position(positions); 
+        });
+    
+    } // END if .anchor-menu .sticky-container EXISTS
+    
+    
+    // Menu stickiness on .resize()
+    $(window).on('resize', function(){
+        if ($( ".anchor-menu .sticky-container" ).length) {
+            menuStickiness();
+        }
+    });
+    
+
+    
+   
+    // Modal functionality
+    // Empty href modal
+    $('a[href=""]').on("click", function(){
+        if (!$(this).parents('.sticky-container').length && !$(this).hasClass("guide_navlink")){
+            $(".modal-wrapper").addClass("active");
+            $(".modal-background").addClass("active");
+        }
+    });
+    
+    $('.inactive-path').on("click", function(){
+        $(".modal-wrapper").addClass("active");
+        $(".modal-background").addClass("active");
+    });
+    
+    $(".modal-close").on("click", function(){
+        $(".modal-wrapper").removeClass("active");
+        $(".modal-background").removeClass("active");
+    });
+
+    $(".modal-background").on("click", function(){
+        $(".modal-wrapper").removeClass("active");
+        $(".modal-background").removeClass("active");
+    });
+    
+    // Search not working modal
+     $(".btn-search").on("click", function(){
+        $(".modal-wrapper").addClass("active");
+        $(".modal-background").addClass("active");
+    });
+    
+    
+}); // END doc ready
+
